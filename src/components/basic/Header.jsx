47b855064963b3b9a4ac17700ab91b329744a7bb
laana-vi <lana.vicovac@gmail.com>
2021-03-03 14:09:43 +0100

    broken home link wasn't broken

--- src/components/basic/Header.jsx
+++ src/components/basic/Header.jsx
@@ -13,7 +13,7 @@ const Header = ({ user }) => {
             {
                 user ?
                     <nav className='header-container'>
-                            <Link to='/home' onClick={() => setOpen(!open)}><img className='pic' src='https://res.cloudinary.com/dpj7zvqzs/image/upload/v1614626528/media/posts/logo_tnx6vv.png' alt="" /></Link>
+                            <NavLink exact to='/home'><img className='pic' src='https://res.cloudinary.com/dpj7zvqzs/image/upload/v1614626528/media/posts/logo_tnx6vv.png' alt="" /></NavLink>
                         <ul className="nav-links">
                             <li><NavLink activeClassName="navbar__link--active" exact className="item" to='/admin' onClick={() => setOpen(!open)}>MY POSTS</NavLink></li>
                             <li><NavLink activeClassName="navbar__link--active" exact  className="item" to='/settings' onClick={() => setOpen(!open)}>SETTINGS</NavLink></li>
